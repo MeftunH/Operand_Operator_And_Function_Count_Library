0f1dc8cbc238bb294480917bedd0bab6e18a047c
Maftun Hashimli <maftunhashimli@gmail.com>
2022-04-09 13:22:00 +0300

    Add test class

--- src/pckg/Program.java
+++ src/pckg/Program.java
@@ -1,4 +1,12 @@
 package pckg;
 
+import pckg.Class.Reader;
+
+import java.io.IOException;
+
 public class Program {
+    public static void main(String[] args) throws IOException {
+        Reader reader = new Reader("src/pckg/Testing.java");
+        reader.readFromFile("src/pckg/Testing.java");
+    }
 }
