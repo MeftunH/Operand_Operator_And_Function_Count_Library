a3e0e377b4ecc7c5ff438237ee9fafeaea7d3daa
Maftun Hashimli <maftunhashimli@gmail.com>
2022-04-12 16:15:49 +0300

    set operand type enum

--- src/pckg/Class/Operator.java
+++ src/pckg/Class/Operator.java
@@ -8,27 +8,29 @@ import java.util.regex.Pattern;
 public class Operator {
 
     private String input;
-    String[] singleOperators = {"+", "-", "*", "/", "%", "&", "|", "^", "=", "<", ">", "!"};
+    String[] SINGULAROperators = {"+", "-", "*", "/", "%", "&", "|", "^", "=", "<", ">", "!"};
     String[] dualOperators = {"++", "--", "+=", "-=", "*=", "/=", "%=", "&=", "&&", "^=", "<=", ">=" ,"|=", "==", "!=", "||"};
-
-    private int totalNumberOfSingleOperators;
+    OperatorType operatorType;
+    
+    
+    private int totalNumberOfSINGULAROperators;
     private int totalNumberOfDualOperators;
     private int totalNumberOfOperators;
 
-    public int getTotalNumberOfSingleOperators(String input){
-        return totalNumberOfSingleOperators;
+    public int getTotalNumberOfSINGULAROperators(String input){
+        return totalNumberOfSINGULAROperators;
     }
 
-    public void setTotalNumberOfSingleOperators(int totalNumberOfSingleOperators) {
-        this.totalNumberOfSingleOperators = totalNumberOfSingleOperators;
+    public void setTotalNumberOfSINGULAROperators(int totalNumberOfSINGULAROperators) {
+        this.totalNumberOfSINGULAROperators = totalNumberOfSINGULAROperators;
     }
 
-    public void setTotalNumberOfSingleOperators(String input) {
-        this.totalNumberOfSingleOperators = getOperatorCount(input, singleOperators);
+    public void setTotalNumberOfSINGULAROperators(String input) {
+        this.totalNumberOfSINGULAROperators = getOperatorCount(input, OperatorType.SINGULAR.elements,1);
     }
 
-    public int getTotalNumberOfSingleOperators() {
-        return totalNumberOfSingleOperators;
+    public int getTotalNumberOfSINGULAROperators() {
+        return totalNumberOfSINGULAROperators;
     }
 
     public int getTotalNumberOfDualOperators() {
@@ -40,24 +42,24 @@ public class Operator {
     }
 
     public void setTotalNumberOfDualOperators(String input) {
-        this.totalNumberOfDualOperators = getOperatorCount(input, dualOperators);;
+        this.totalNumberOfDualOperators = getOperatorCount(input, OperatorType.Dual.elements,2);;
     }
 
     public int getTotalNumberOfOperators() {
-        return getTotalNumberOfDualOperators() + getTotalNumberOfSingleOperators();
+        return getTotalNumberOfDualOperators() + getTotalNumberOfSINGULAROperators();
     }
 
     public void setTotalNumberOfOperators(int totalNumberOperators) {
         this.totalNumberOfOperators = totalNumberOperators;
     }
 
-    private int getOperatorCount(String input, String[] operators) {
+    private int getOperatorCount(String input, String[] operators, int numberOfJump) {
         int operatorCounter = 0;
         for (String operator : operators) {
             int index = input.indexOf(operator);
             while (index != -1) {
                 operatorCounter++;
-                input = input.substring(index + 1);
+                input = input.substring(index + numberOfJump);
                 index = input.indexOf(operator);
             }
         }
@@ -65,12 +67,12 @@ public class Operator {
     }
 
     public int getTotalNumberOfOperators(String input){
-        int totalNumberOfSingleOperators = getOperatorCount(input, singleOperators);
-        int totalNumberOfDualOperators = getOperatorCount(input, dualOperators);
-        int totalNumberOfOperators = totalNumberOfSingleOperators + totalNumberOfDualOperators;
+        int totalNumberOfSINGULAROperators = getOperatorCount(input, SINGULAROperators,1);
+        int totalNumberOfDualOperators = getOperatorCount(input, dualOperators,2);
+        int totalNumberOfOperators = totalNumberOfSINGULAROperators + totalNumberOfDualOperators;
         setTotalNumberOfOperators(totalNumberOfOperators);
         System.out.println("Operator{" +
-                "totalNumberOfSingleOperators=" + totalNumberOfSingleOperators +
+                "totalNumberOfSINGULAROperators=" + totalNumberOfSINGULAROperators +
                 ", totalNumberOfDualOperators=" + totalNumberOfDualOperators +
                 ", totalNumberOperators=" + totalNumberOfOperators +
                 '}');
---
+++ src/pckg/Class/OperatorType.java
@@ -0,0 +1,54 @@
+package pckg.Class;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum OperatorType {
+
+
+
+    SINGULAR_DIGITAL("SINGULAR_DIGITAL",2, new String[]{"+", "-", "*", "/", "%", "&", "|", "^", "="}),
+    SINGULAR_RELATIONAL("SINGULAR_RELATIONAL",2,new String[]{"<", ">"}),
+    SINGULAR_LOGICAL("SINGULAR_LOGICAL",2,new String[]{"!"}),
+    SINGULAR("SINGULAR",2,new String[]{"+", "-", "*", "/", "%", "&", "|", "^", "=", "<", ">", "!"}),
+    DUAL_DIGITAL("DUAL_DIGITAL",1,new String[]{"++", "--", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^="}),
+    DUAL_RELATIONAL("DUAL_RELATIONAL",1,new String[]{"<=", ">=", "==", "!="}),
+    DualLogical("DualLogical",1,new String[]{"&&", "||"}),
+    Dual("Dual",1,new String[]{"++", "--", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=","<=", ">=", "==", "!=","&&", "||"});
+
+
+    private static final Map<String, OperatorType> BY_LABEL = new HashMap<>();
+    private static final Map<Integer, OperatorType> BY_ITERATION_NUMBER = new HashMap<>();
+    private static final Map<String[], OperatorType> BY_ELEMENTS = new HashMap<>();
+
+    static {
+        for (OperatorType operatorType : values()) {
+            BY_LABEL.put(operatorType.label, operatorType);
+            BY_ITERATION_NUMBER.put(operatorType.numberOfIteration, operatorType);
+            BY_ELEMENTS.put(operatorType.elements, operatorType);
+        }
+    }
+
+    public final String label;
+    public final int numberOfIteration;
+    public final String[] elements;
+
+    OperatorType(String label, int numberOfIteration, String[] elements) {
+        this.label = label;
+        this.numberOfIteration = numberOfIteration;
+        this.elements = elements;
+    }
+
+    public static OperatorType valueOfLabel(String label) {
+        return BY_LABEL.get(label);
+    }
+
+    public static OperatorType valueOfElements(String[] elements) {
+        return BY_ELEMENTS.get(elements);
+    }
+
+    public static OperatorType valueOfIterationNumber(int numberOfIteration) {
+        return BY_ITERATION_NUMBER.get(numberOfIteration);
+    }
+
+}
